cbba59aee1f43b0f9fa1577b45a2c39cfc13d6a4
TheOG-2670 <ogolan3589@gmail.com>
2023-05-30 18:00:31 -0400

    read directories in path from command line argument

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <cstdio>
+#include <dirent.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("hello from %s!\n", "SystemStats");
+    DIR* dir;
+    struct dirent* ent;
+
+    const char *dirName = argv[1];
+    dir = opendir(dirName);
+
+    //if the directory doesn't exist, return error
+    if (!dir) {
+        perror("cannot read directory");
+        return -1;
+    }
+    
+    //while the current directory entry is not NULL, read it and print its name
+    while (ent = readdir(dir)) {
+        printf("%s\n", ent->d_name);
+    }
+
     return 0;
 }
