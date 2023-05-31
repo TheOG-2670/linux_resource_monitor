d7844cb64bff201bf74372a56e45896b2fd92678
TheOG-2670 <ogolan3589@gmail.com>
2023-05-30 22:00:41 -0400

    add regex matching for 'cpu' lines in '/proc/stat' file

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <regex>
 
 int main(int argc, char **argv)
 {
-    std::string fileName= argv[1];
+    std::string fileName = "/proc/stat";
     std::cout << "file is: " << fileName << std::endl;
+
+    std::regex re("(cpu)(.*)");
 
     //open file
     std::ifstream ifs(fileName);
     if (ifs.is_open()) 
     {
-        //read each line of file while no errors and print the current line
+        //read each line of file while no errors and print the current line if it matches a defined regex pattern
         std::string lineData;
         while (ifs.good())
         {
             std::getline(ifs, lineData);
-            std::cout << lineData << std::endl;
+            if (std::regex_match(lineData, re)) {
+                std::cout << lineData << std::endl;
+            }
         }
     }
+    ifs.close();
 
     return 0;
 }
