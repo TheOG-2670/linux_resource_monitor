241a4cecefd4aaa65d9a907015c4da11f1bae819
TheOG-2670 <ogolan3589@gmail.com>
2023-08-29 16:44:14 -0400

    Update Program.cs

--- ASPNETWebSocketAPI/Program.cs
+++ ASPNETWebSocketAPI/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
@@ -22,11 +21,8 @@ app.Use(async (context, next) =>
     {
         if (context.WebSockets.IsWebSocketRequest)
         {
-            websocketClients.Add(await context.WebSockets.AcceptWebSocketAsync(), null);
-            foreach(KeyValuePair<WebSocket, DataDTO?> websocket in websocketClients)
-            {
-                await Echo(websocket.Key);
-            }
+            var websocket = await context.WebSockets.AcceptWebSocketAsync();
+            await Echo(websocket);
         }
         else
         {
@@ -47,25 +43,23 @@ async Task Echo(WebSocket webSocket)
     DataDTO? r = new DataDTO();
 
     Console.WriteLine("client connected");
+
     do
     {
         result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
         if (result.MessageType == WebSocketMessageType.Text)
         {
             r = JsonSerializer.Deserialize<DataDTO>(Encoding.ASCII.GetString(buffer, 0, result.Count));
-            if (webSocket == websocketClients.FirstOrDefault(x => x.Key== webSocket).Key)
-            {
-                websocketClients.Remove(websocketClients.FirstOrDefault(x => x.Key == webSocket).Key);
-                websocketClients.TryAdd(webSocket, r);
-                Console.WriteLine($"{r}:\nuser id: {r.userId}, message: {r.message}");
-                Console.WriteLine(websocketClients.FirstOrDefault(x => x.Value == r).Value?.ToString());
-                await webSocket.SendAsync(Encoding.ASCII.GetBytes(r.ToString()), WebSocketMessageType.Text, true, CancellationToken.None);
-            }
+
+            Console.WriteLine($"{r}:\nuser id: {r.userId}, message: {r.message}");
+            await webSocket.SendAsync(Encoding.ASCII.GetBytes(r.ToString()), WebSocketMessageType.Text, true, CancellationToken.None);
+
         }
     }
     while (!result.CloseStatus.HasValue);
+
     Console.WriteLine("client disconnected");
-    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "connection terminated", CancellationToken.None);
+    await webSocket.CloseAsync(result.CloseStatus.Value, "connection terminated", CancellationToken.None);
 }
 
 app.Run();
